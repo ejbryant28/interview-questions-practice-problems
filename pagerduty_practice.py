f1d6fc21f7ced0d88244e0f704b9f2586b5570f5
E.J. Bryant <ejbryant28@gmail.com>
2018-11-02 14:16:14 -0700

    practicing regex

---
+++ pagerduty_practice.py
@@ -0,0 +1,22 @@
+
+def reverse(x):
+    """
+    :type x: int
+    :rtype: int
+    """
+    import re
+    num = str(x)
+    pattern = r'\d'
+    
+    num_matches = re.findall(pattern, num)
+    if num_matches[-1] == '0':
+        num_matches = num_matches[:-1]
+
+    if not num_matches:
+        return 0
+                             
+    if num[0] == '-':
+        num_matches.append('-')
+    new_num = ''.join(reversed(num_matches))
+                             
+    return int(new_num)
