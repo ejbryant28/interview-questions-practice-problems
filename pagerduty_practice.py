06aa2be1ff81dcdba1e1b1c06a5ee4d46553a350
E.J. Bryant <ejbryant28@gmail.com>
2018-11-06 12:33:14 -0800

    Redid reverse int problem

--- pagerduty_practice.py
+++ pagerduty_practice.py
@@ -227,7 +227,198 @@ assert(lengthOfLastWord('')==0)
 assert(lengthOfLastWord('hello world') == 5)
 
 
+def plusOne(digits):
+    """
+    :type digits: List[int]
+    :rtype: List[int]
+    """
+    
+    num = 0
+    r = 1
+    
+    for n in range(1, len(digits)+1):
+        num += digits[-n] * r
+        r *= 10
+        
+    num += 1
+    new_digits = []
+    
+    j = 10
+    while num:
+        
+        remainder = num % j
+        dig = remainder * 10 // j
+        
+        new_digits.insert(0, dig)
+        num -= remainder
+        j *= 10
+        
+    return new_digits
+
+assert(plusOne([1, 1, 1]) == [1, 1, 2])
+assert(plusOne([1, 1, 9]) == [1, 2, 0])
+assert(plusOne([9, 9]) == [1, 0, 0])
+
+
+def romanToInt(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    
+    roman_to_int = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    
+    ints = []
+    
+    for roman in s:
+        ints.append(roman_to_int[roman])
+
+    check = list(sorted(ints))
+    check = list(reversed(check))
+    # print(check.reverse)
+        
+    if check == ints:
+        return sum(ints)
+    
+    num = 0
+    
+    #[1000, 100, 1000, 10, 100, 1, 5]
+    #[[1000], [100, 1000], [10, 100], [1, 5]]
+    #[1000, 900, 90, 4]
+
+    #[10, 100, 5, 1]
+    #[[10, 100], [5] [1]]
+    
+    grouped = []
+    
+    while ints:
+
+        if len(ints) == 1:
+            n = ints.pop()
+            grouped.append([n])
+
+        elif ints[-1] <= ints[-2]:
+            n = ints.pop()
+            grouped.append([n])
+
+        else:
+            group = ints[-2:]
+            grouped.append(group)
+            ints[-2:] = []
+
+    for i, group in enumerate(grouped):
+
+        if len(group) == 1:
+            grouped[i] = group[0]
+
+        else:
+            num = group[1] - group[0]
+            grouped[i] = num
+
+
+
+
+    return sum(grouped)
+    
+assert(romanToInt('III') == 3)
+assert(romanToInt('IV') == 4)
+assert(romanToInt('MCMXCIV') == 1994)
+assert(romanToInt('XIII') == 13)
+assert(romanToInt('XLVIII') == 48)
+
+def searchInsert(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    
+    #[1, 2, 3, 4], 5 --> 4
+    #[1, 2, 3, 4], 4 --> 3
+    #[1, 4, 5, 6], 3 --> 1
+    #[], 2 --> 0
+    
+    if not nums or target <= nums[0]:
+        return 0
+    
+    if target > nums[-1]:
+        return len(nums)
+    
+    if target == nums[-1]:
+        return len(nums) - 1
+    
+    for i, num in enumerate(nums):
+        
+        if num == target:
+            return i
+        
+        elif num > target:
+            return i
+
+assert(searchInsert([1, 3, 5, 6], 2) == 1)
+
+
+def isPalindrome(s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+    
+    #'A man, a plan, a canal: Panama' --> True
+    #'amanaplanacanalpanama' == reverse?
+    
+    joined = []
+    
+    for letter in s:
+        if letter.isalpha():
+            joined.append(letter.lower())
+    
+    if joined == list(reversed(joined)):
+        return True
+    
+    return False
+
+# print(isPalindrome('OP'))
+# print(isPalindrome(''))
+# print('passed!')
+
+import math
+def rev_int(n):
+
+    #n, and new_num
+    #n % 10, add the remainder times the total magnitude to new_num
+    #multiply the 10 by 10, divide total magnitude by 10
+    if n < 0:
+        neg = True
+        n = abs(n)
+
+    else:
+        neg = False
+
+    rev = 0
+    while n > 0:
+        pop = n % 10
+        n -= pop
+        n /= 10
+
+        rev = rev * 10 + pop
+
+    if neg:
+        return int(rev) * -1
+
+    return int(rev)
+
+
+assert(rev_int(123)== 321)
+assert(rev_int(1)==1)
+assert(rev_int(-123)==-321)
+assert(rev_int(0)==0)
+assert(rev_int(12340)==4321)
+assert(rev_int(1234)==4321)
+
 print('passed!')
+
+# print(rev_int(123))
 
 
 
